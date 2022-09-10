dd7a6cd4e96e9c2a42c5f4b093b746ae0018a582
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2022-09-10 10:15:40 +0500

    I have created a cron-job to fetch the data every day and I have defined a function in src/index.js to get the product data and  I have deployed the strapi-pmi-backend on the Postgres Database in Heroku.

--- strapi-pmi/config/database.js
+++ strapi-pmi/config/database.js
@@ -1,11 +1,15 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
+  defaultConnection: "default",
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "ec2-44-207-253-50.compute-1.amazonaws.com"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "d4t3id3gh4j4bf"),
+      user: env("DATABASE_USERNAME", "mjekafvvbatrrg"),
+      password: env("DATABASE_PASSWORD"),
+      schema: env("DATABASE_SCHEMA", "public"),
+      ssl: { rejectUnauthorized: false },
     },
-    useNullAsDefault: true,
-  },
+  }
 });
